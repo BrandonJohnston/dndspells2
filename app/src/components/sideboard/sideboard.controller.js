6dac05bd11701faba617c3618dbcd9051aff2ad3
Brandon Johnston <bjohnst@us.ibm.com>
2016-06-07 12:56:16 -0500

    updates to sideboard and top nav

--- app/src/components/sideboard/sideboard.controller.js
+++ app/src/components/sideboard/sideboard.controller.js
@@ -10,22 +10,43 @@ angular
 sideboardController.$inject = [
     '$rootScope',
     '$scope',
-    '$log'
+    '$log',
+    '$state'
 ];
 
 
-function sideboardController($rootScope, $scope, $log) {
+function sideboardController($rootScope, $scope, $log, $state) {
 
     var vm = this;
     $log.debug('sideboardController');
 
 
     // Setup functions
-    //vm.toggleSideboard = toggleSideboard;
+    vm.navigate = navigate;
 
 
     // Setup variables
+    vm.stateDetails = null;
     vm.isSideboardOpen = false;
+
+
+    /*
+     * Navigate to new state when option is clicked in sideboard
+     */
+    function navigate(newState) {
+
+        if (newState !== vm.stateDetails.name) {
+            $state.go(newState);
+        }
+    }
+
+
+    /*
+     * Update the state when it changes
+     */
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        vm.stateDetails = toState;
+    });
 
 
     /*
